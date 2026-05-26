06a7d6af865c171324ed01aaf72471411d4b821e
Punit Deotale <punitdeotale@gmail.com>
2026-05-25 18:55:18 -0700

    Revert to Blowfish theme and refine neonforest color scheme

--- themes/flavor/static/js/main.js
+++
@@ -1,6 +0,0 @@
-// Close mobile nav on link click
-document.querySelectorAll('.site-nav a').forEach(link => {
-  link.addEventListener('click', () => {
-    document.querySelector('.site-nav').classList.remove('open');
-  });
-});
